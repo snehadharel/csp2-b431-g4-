3e2d9be437d58c0287e0333840a8b61f449a2bd4
snehadharel <166776135+snehadharel@users.noreply.github.com>
2024-09-10 10:29:12 +0545

    Update orderRoutes.js

--- order/orderRoutes.js
+++ order/orderRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const orderController = require("../controllers/orderController");
-const { protect, admin } = require("../authGuard");
+const orderController = require("../controller/ordercontroller");
+const { protect, admin } = require("../auth");
 
 // Route to get user's orders
-router.get("/my-orders", protect, orderController.getUserOrders);
+router.get("/my-orders", protect, ordercontroller.getUserOrders);
 
 // Route to get all orders (Admin only)
-router.get("/all-orders", protect, admin, orderController.getAllOrders);
+router.get("/all-orders", protect, admin, ordercontroller.getAllOrders);
 
 // Route to create a new order
-router.post("/checkout", protect, orderController.createOrder);
+router.post("/checkout", protect, ordercontroller.createOrder);
 
 module.exports = router;
