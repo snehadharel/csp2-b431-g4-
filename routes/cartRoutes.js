7dc22e6b1b1dd9a0206ab2219e8da885ddb4d759
snehadharel <166776135+snehadharel@users.noreply.github.com>
2024-09-10 10:56:46 +0545

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cartController = require('../controllers/cartController');
+const cartController = require('../controller/cartController');
 const { protect } = require('../auth');
 
 
