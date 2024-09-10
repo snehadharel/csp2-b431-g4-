286f768808421dc6362ae3abb28158dccdc241d1
snehadharel <166776135+snehadharel@users.noreply.github.com>
2024-09-10 10:53:49 +0545

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const cartController = require('../controller/Cartcontroller');
+const cartController = require('../controllers/cartController');
 const { protect } = require('../auth');
 
 
 // Route to add to cart
-router.post('/add-to-cart', protect, Cartcontroller.addToCart);
+router.post('/add-to-cart', protect, cartController.addToCart);
 // Route to retrieve user's cart
-router.get('/get-cart', protect, Cartcontroller.getCart);
+router.get('/get-cart', protect, cartController.getCart);
 
 // Route to update product quantity in cart
-router.patch('/update-cart-quantity', protect, Cartcontroller.updateCartQuantity);
+router.patch('/update-cart-quantity', protect, cartController.updateCartQuantity);
 
 // Route to remove item from cart
-router.patch('/:productId/remove-from-cart', protect, Cartcontroller.removeFromCart);
+router.patch('/:productId/remove-from-cart', protect, cartController.removeFromCart);
 
 // Route to clear cart
-router.put('/clear-cart', protect, Cartcontroller.clearCart);
+router.put('/clear-cart', protect, cartController.clearCart);
 
 module.exports = router;
